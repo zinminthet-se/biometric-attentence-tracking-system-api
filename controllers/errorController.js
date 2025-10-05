18f39365b20407b83f780cd01cb4cd539bb445fe
zinminthet <zinminthet.se@gmail.com>
2025-10-05 22:19:02 +0630

    feat: add error format for production and development

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,8 +1,9 @@
 const sendErrorProduction = (err, req, res, next) => {
   // operational errors
   if (err.isOperational) {
+    console.log(err);
     return res.status(err.statusCode).json({
-      error: { ...err },
+      error: { message: err.message, ...err },
     });
   }
 
@@ -13,9 +14,21 @@ const sendErrorProduction = (err, req, res, next) => {
   });
 };
 
-const sendErrorDevelopement = (err, req, res, next) => {};
+const sendErrorDevelopement = (err, req, res, next) => {
+  if (req.originalUrl.startsWith("/api")) {
+    return res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+      error: err,
+      stack: err.stack,
+    });
+  }
+};
 
 exports.globalErrorController = (err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || "error";
+
   if (process.env.NODE_ENV == "development") {
     sendErrorDevelopement(err, req, res, next);
   } else if (process.env.NODE_ENV == "production") {
